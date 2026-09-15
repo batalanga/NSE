30cde04b6ebaa4ae4ed215732f4808997b717323
batalanga <batalanga@batalanga.batalanga>
2026-09-16 01:46:36 +0200

    for you

---
+++ getmods.php
@@ -0,0 +1,8 @@
+nRows=6
+&strModName0=NSEg&strModURL0=Mods/NeoScavExtended/NSEgame
+&strModName1=NSEb&strModURL1=Mods/NeoScavExtended/NSEbattle
+&strModName2=NSEf&strModURL2=Mods/NeoScavExtended/NSEfashion
+&strModName3=NSEa&strModURL3=Mods/NeoScavExtended/NSEaid
+&strModName4=NSE&strModURL4=Mods/NeoScavExtended/NSExtended
+&strModName5=0&strModURL5=Mods/NeoScavExtended/NSEoverride
+&strModName6=NSEtT&strModURL6=Mods/NeoScavExtended/NSEtestTools
